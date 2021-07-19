7ef604a94abe6bd2d9030786dbc9d9375b6cf8d2
SaekoKuroda <85662906+SaekoKuroda@users.noreply.github.com>
2021-07-19 14:53:11 +0900

    TODO追加1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+const onClickAddTodo = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // divタグ生成
+  const div = document.createElement("div");
+  alert(inputText);
+};
+
+document
+  // html上で付与したidを持つものに対して
+  .getElementById("add-button")
+  // clickイベントを付与する
+  .addEventListener("click", () => onClickAddTodo());
